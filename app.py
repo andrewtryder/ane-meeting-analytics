4aa987704f673c6b57a44e8fba7cd43a8d23a861
Andrew Ryder <1811535+andrewtryder@users.noreply.github.com>
2023-04-13 08:20:07 -0400

    Update the meetings dictionary

--- app.py
+++ app.py
@@ -10,14 +10,14 @@ meetings = ['meeting1', 'meeting2', 'meeting3']
 # Route to display list of meetings
 @app.route('/')
 def index():
-    meetings = [{'meeting_id': '87270619114', 'date': 'April 2023', 'title': 'Damon'},
-                {'meeting_id': '85923120072', 'date': 'March 2023', 'title': ''},
-                {'meeting_id': '88189866002', 'date': 'February 2023', 'title': ''},
-                {'meeting_id': '81555879346', 'date': 'January 2023', 'title': ''},
-                {'meeting_id': '89033277712', 'date': 'December 2022', 'title': ''},
-                {'meeting_id': '83479774433', 'date': 'November 2022', 'title': ''},
-                {'meeting_id': '81355532493', 'date': 'October 2022', 'title': ''},
-                {'meeting_id': '82792015750', 'date': 'September 2022', 'title': ''},
+    meetings = [{'meeting_id': '87270619114', 'date': 'April 2023', 'title': 'Agile, The Life-Size Board Game!'},
+                {'meeting_id': '85923120072', 'date': 'March 2023', 'title': 'S.E.E.ing Your Projects Needs: Creating a Psychological Safety Plan'},
+                {'meeting_id': '88189866002', 'date': 'February 2023', 'title': 'Everyone is...an Agile Coach'},
+                {'meeting_id': '81555879346', 'date': 'January 2023', 'title': 'How to Minimize Stress in Corporate America'},
+                {'meeting_id': '89033277712', 'date': 'December 2022', 'title': 'ANE and Agile - Our Past and Future'},
+                {'meeting_id': '83479774433', 'date': 'November 2022', 'title': 'Wickd Agility - Agile and Planetary Challenges by Damon Edwards'},
+                {'meeting_id': '81355532493', 'date': 'October 2022', 'title': 'The Fine Art of Zero F***s Given by Jessica Katz'},
+                {'meeting_id': '82792015750', 'date': 'September 2022', 'title': 'Putting the Sec in DevOps by Sean Wright'},
                 {'meeting_id': '85784277169', 'date': 'August 2022', 'title': 'Get Focused and Get Things Done: 6 Principles of Time Management by Jessica Katz'},
                 {'meeting_id': '82588783947', 'date': 'July 2022', 'title': 'FAST: An Innovative Way to Scale by James Shore'},
                 {'meeting_id': '82266617679', 'date': 'June 2022', 'title': 'My Agile Journey by Kevin Ball'},
